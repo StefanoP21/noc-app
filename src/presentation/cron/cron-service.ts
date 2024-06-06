571aa3f6007fa9265c9bb461a72b5a9cca5ef173
Stefano <stefanop21@outlook.es>
2024-06-06 16:57:21 -0500

    new: CronService

---
+++ src/presentation/cron/cron-service.ts
@@ -0,0 +1,17 @@
+import { CronJob } from 'cron';
+
+type CronTime = string | Date;
+type OnTick = () => void;
+
+export class CronService {
+  static createJob(cronTime: CronTime, onTick: OnTick): CronJob {
+    const job = CronJob.from({
+      cronTime,
+      onTick,
+      start: true,
+      timeZone: 'default',
+    });
+
+    return job;
+  }
+}
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,11 @@
+import { CronService } from './cron/cron-service';
+
 export class ServerApp {
   public static start() {
     console.log('Server started!');
+
+    CronService.createJob('*/5 * * * * *', () => {
+      console.log('Cron job running every 5 seconds', new Date());
+    });
   }
 }
