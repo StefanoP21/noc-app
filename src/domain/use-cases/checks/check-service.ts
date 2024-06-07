3b7bd3effad24807b4a06d9ff2b90ba649196cf0
Stefano <stefanop21@outlook.es>
2024-06-07 00:05:19 -0500

    update: probar la implementación

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -5,8 +5,8 @@ interface CheckServiceUseCase {
   execute: (url: string) => Promise<boolean>;
 }
 
-type SuccessCallback = () => void;
-type ErrorCallback = (error: string) => void;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = ((error: string) => void) | undefined;
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
@@ -30,18 +30,18 @@ export class CheckService implements CheckServiceUseCase {
       );
 
       this.logRepository.saveLog(log);
-      this.successCallback();
+      this.successCallback && this.successCallback();
 
       return true;
     } catch (error) {
       const errorMessage = `${error}`;
       const log = new LogEntity(
-        LogSeverityLevel.low,
+        LogSeverityLevel.high,
         `Error on check service ${url}`
       );
 
       this.logRepository.saveLog(log);
-      this.errorCallback(errorMessage);
+      this.errorCallback && this.errorCallback(errorMessage);
 
       return false;
     }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,14 +1,21 @@
 import { CheckService } from '../domain/use-cases/checks/check-service';
+import { FileSystemDataSource } from '../infraestructure/datasources/file-system.datasource';
+import { LogRepositoryImpl } from '../infraestructure/repositories/log.repository.impl';
 import { CronService } from './cron/cron-service';
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDataSource()
+);
 
 export class ServerApp {
   public static start() {
     console.log('Server started!');
 
-    const url = 'https://www.isdin.com/es-PE/';
+    const url = 'http://localhost:3000/';
 
     CronService.createJob('*/5 * * * * *', () => {
       new CheckService(
+        fileSystemLogRepository,
         () => console.log(`Success: ${url}`),
         (error) => console.log(`Error: ${error}`)
       ).execute(url);
