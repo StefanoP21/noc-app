05debbab3e87b51193a9e510e9bc14d26d6eef10
Stefano <stefanop21@outlook.es>
2024-06-06 23:10:36 -0500

    update: FileSystem - GetLogs

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -14,4 +14,18 @@ export class LogEntity {
     this.message = message;
     this.createdAt = new Date();
   }
+
+  static fromJson = (json: string): LogEntity => {
+    const { level, message, createdAt }: LogEntity = JSON.parse(json);
+
+    //* Check if the required fields are present
+    if (!level || !message || !createdAt) {
+      throw new Error('Invalid log entity');
+    }
+
+    const log = new LogEntity(level, message);
+    log.createdAt = new Date(createdAt);
+
+    return log;
+  };
 }
